ce4895a69a4a5e33e21c6f272400ab222fa66b49
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-01-21 15:47:19 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,13 @@
+print('hello world')
+
+message = "Python For Crypto Bot           LAST"
+
+print("message : " + message)
+print(len(message)) #length of the string
+print(message[1]) #second character
+print(message[0]) #first character
+print(message[-1])  #last character
+print(message[0:4]) #range #4 excluded 0 included
+print(message[6:9])
+print(message.lower()) #all lowercase character
+print(message.upper()) #all uppercase character
