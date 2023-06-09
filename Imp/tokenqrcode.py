1f333733e827cd90fcafa68d8a8d8d86750e33db
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-09 21:55:59 +0530

    Update tokenqrcode.py

--- Imp/tokenqrcode.py
+++ Imp/tokenqrcode.py
@@ -1,1 +1,11 @@
  qr "https://www.fiverr.com/s/zLLjVd" token.png
+ 
+ 
+ import qrcode
+
+img = qrcode.make('Fiverr')
+img.save('fiverr.png')
+
+link = qrcode.make('https://www.fiverr.com/s/pLLraR')
+link.save('token.png')
+
