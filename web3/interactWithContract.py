f7a2de13795302a0f36a75bc576f46d0488d884c
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-17 02:01:03 +0530

    Create interactWithContract.py

---
+++ web3/interactWithContract.py
@@ -0,0 +1,30 @@
+from web3 import Web3
+from colorama import Fore
+# ------------------------------------------------------------------------------------
+
+INFURA = "https://mainnet.infura.io/v3/0a7dfdsfsdf13031"
+web3 = Web3(Web3.HTTPProvider(INFURA))
+print(f'{Fore.YELLOW} Connected: {Fore.WHITE} {web3.is_connected()}')
+
+# ---------------------------------------------------------------------------------
+
+target_address = web3.toChecksumAddress("0x514910771AF9Ca656af840dff83E8264EcF986CA")
+target_ABI = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transferAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"data","type":"bytes"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]'
+target = web3.eth.contract(address=target_address, abi=target_ABI)
+
+# ----------------------------------------------------------------------------------
+
+name = target.functions.name().call()
+symbol = target.functions.symbol().call()
+total_supply = target.functions.totalSupply().call()
+total_supply_fromWei = web3.fromWei(target.functions.totalSupply().call(), 'ether')
+decimals = target.functions.decimals().call()
+
+print('Name: ', name)
+print('Symbol: ', symbol)
+print('Total Supply: ', total_supply / 10 ** decimals)
+print('Decimals: ', decimals)
+print('Total Supply fromWei: ', total_supply_fromWei)
+
+# -------------------------------------------------------------------------------------
+
