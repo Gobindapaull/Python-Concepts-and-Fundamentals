d13296fcecf1f0ad182a878dc33170f8ec58c68b
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-18 05:35:39 +0530

    Create USDTTx.py

---
+++ web3/USDTTx.py
@@ -0,0 +1,59 @@
+from web3 import Web3
+import json
+
+bsc_url = "https://bsc-dataseed.binance.org"
+web3 = Web3(Web3.HTTPProvider(bsc_url))
+
+print(web3.is_connected())
+
+# -----------------------------------------------------------------
+
+contract_address = web3.toChecksumAddress("0x55d398326f99059fF775485246999027B3197955")
+contract_abi = '[{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"_decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"mint","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]'
+
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
+# ----------------------------------------------------------------------
+sender = web3.to_checksum_address("")
+private_key = ""
+receiver = web3.to_checksum_address("")
+
+# --------------------------------------------------------------------------------
+
+name = contract.functions.name().call()
+symbol = contract.functions.symbol().call()
+total_supply = contract.functions.totalSupply().call()
+total_supply_fromWei = web3.fromWei(total_supply, 'ether')
+decimals = contract.functions.decimals().call()
+balanceOf = contract.functions.balanceOf(sender).call()
+
+
+print('Name: ', name)
+print('Symbol: ', symbol)
+print('Total Supply: ', total_supply / 10 ** decimals)
+print('Decimals: ', decimals)
+print('Total Supply fromWei: ', total_supply_fromWei)
+print('balanceOf sender: ', web3.from_wei(balanceOf, 'ether'))
+
+# ----------------------------------------------------------------
+
+nonce = web3.eth.get_transaction_count(sender)
+amount = web3.to_wei(0.05, 'ether') # usdt amount = 0.05
+
+tx = {
+    'nonce': nonce,
+    'gas': 500000,
+    'gasPrice': web3.to_wei('7', 'gwei'),
+    'chainId': 56
+}
+
+token_tx = contract.functions.transfer(receiver, amount).build_transaction(tx)
+
+# -----------------------------------------------------------------
+
+signed_tx = web3.eth.account.sign_transaction(token_tx, private_key)
+tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+print(f'https://bscscan.com/tx/'+web3.to_hex(tx_hash))
+# https://bscscan.com/tx/0x59cdb46b5cc82a4e8179f1eac01fb30329be69e3ac4aa63742494bb52f4ad0dd
+
