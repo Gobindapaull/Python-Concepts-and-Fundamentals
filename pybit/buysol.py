0116dd225fd19574733e48f5810a78afdbc40856
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-06-29 16:42:12 +0530

    Create buysol.py

---
+++ pybit/buysol.py
@@ -0,0 +1,27 @@
+from pybit.unified_trading import HTTP
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+apikey=os.getenv("API_KEY")
+secretkey=os.getenv("SECRET_KEY")
+
+session = HTTP(
+    testnet=True, # real account
+    api_key=apikey,
+    api_secret=secretkey
+)
+
+try:
+    order = session.place_order(
+        category="spot",
+        symbol="SOLUSDT",
+        side="BUY",
+        orderType="Market",
+        qty=5
+    )
+    print(order)
+except Exception as e:
+    print("Error Fetching balance: ", e)
+
