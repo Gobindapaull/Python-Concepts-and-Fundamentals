6bc8a1f0d706e11dd3de907c615026841d6b4707
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-09-26 16:34:07 +0530

    Create strip.py

---
+++ 2025/September/strip.py
@@ -0,0 +1,8 @@
+original_string = "   Hello, World      "
+remove_white_space = original_string.strip()
+
+review = "!!!!!hi!!!!!!"
+remove_exclamation = review.strip("!")
+
+print(remove_white_space) # Hello, World
+print(remove_exclamation) # hi
